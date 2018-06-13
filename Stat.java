9c4ea8fcda67b8e67152056bc9feca4295ffb8a8
AnmoLSin <anisaggu8@gmail.com>
2018-06-13 12:11:41 +0530

    first commit1

---
+++ Stat.java
@@ -0,0 +1,19 @@
+class Stat{
+   static int num;
+   static String mystr;
+   static{
+      System.out.println("Static Block 1");
+      num = 68;
+      mystr = "Block1";
+  } 
+  static{
+      System.out.println("Static Block 2");
+      num = 98;
+      mystr = "Block2";
+  }
+  public static void main(String args[])
+  {
+      System.out.println("Value of num is "+num);
+      System.out.println("Value of mystr is "+mystr);
+   }
+}
